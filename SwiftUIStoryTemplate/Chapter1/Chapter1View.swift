2fb6115a95bdce5be3ba1a6956a0b90da986cf47
Alonso del Arte <alonso.delarte@gmail.com>
2023-11-16 16:55:57 -0500

    Added a VStack

--- SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
+++ SwiftUIStoryTemplate/Chapter1/Chapter1View.swift
@@ -2,9 +2,11 @@ import SwiftUI
 
 struct Chapter1View: View {
     var body: some View {
-        Text("Chapter 1").font(.largeTitle).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-        Text("Given that you're a friend of the Williams family, you know that Abigail Williams was born blind on May 10, 2005.").font(.title).contentMargins(1)
-        Text("Now she's 18-years-old and her older sister Michelle has been going to university remotely.").font(.title).contentMargins(1)
+        VStack {
+            Text("Chapter 1").font(.largeTitle).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("Given that you're a friend of the Williams family, you know that Abigail Williams was born blind on May 10, 2005.").font(.title).contentMargins(1)
+            Text("Now she's 18-years-old and her older sister Michelle has been going to university remotely.").font(.title).contentMargins(1)
+        }
     }
 }
 
