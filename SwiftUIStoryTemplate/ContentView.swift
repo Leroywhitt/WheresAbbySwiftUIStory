1d4f0dfce000487a7360e78b5669af824c2a29dc
Leroy Whitt <lwhitt23@msu.idserve.net>
2023-11-16 22:39:32 -0500

    changed sf symbols

--- SwiftUIStoryTemplate/ContentView.swift
+++ SwiftUIStoryTemplate/ContentView.swift
@@ -8,23 +8,23 @@ struct ContentView: View {
             TabView {
                 Chapter1View()
                     .tabItem {
-                        Label("Chapter 1", systemImage: "1.circle")
+                        Label("Chapter 1", systemImage: "book.pages.fill")
                     }
                 Chapter2View()
                     .tabItem {
-                        Label("Chapter 2", systemImage: "2.circle")
+                        Label("Chapter 2", systemImage: "book.pages.fill")
                     }
                 Chapter3View()
                     .tabItem {
-                        Label("Chapter 3", systemImage: "3.circle")
+                        Label("Chapter 3", systemImage: "book.pages.fill")
                     }
                 Chapter4View()
                     .tabItem {
-                        Label("Chapter 4", systemImage: "4.circle")
+                        Label("Chapter 4", systemImage: "book.pages.fill")
                     }
                 Chapter5View()
                     .tabItem {
-                        Label("Chapter 5", systemImage: "5.circle")
+                        Label("Chapter 5", systemImage: "book.pages.fill")
                     }
                     .padding()
                 
